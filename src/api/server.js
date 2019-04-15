5736cf35b008b90849e828f302f03fa89cb55a4e
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-15 17:09:35 +0100

    set response message for home route

--- src/api/server.js
+++ src/api/server.js
@@ -8,4 +8,8 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'welcome to the jump-tracker api' });
+});
+
 module.exports = server;
