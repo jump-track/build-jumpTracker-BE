1f08f2a219448d2700cb733a80268bc6f639e538
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-18 22:09:43 +0100

    seed data

--- src/data/seeds/001-users.js
+++ src/data/seeds/001-users.js
@@ -1,5 +1,8 @@
-exports.seed = knex =>
-  knex('users')
+const bcrypt = require('bcryptjs');
+exports.seed = async knex => {
+  const password = await bcrypt.hashSync('tenzing', 1);
+  return knex('users')
     .insert([
-      { username: 'admin', password: 'admin', height: 175, jump_height: 40 }
+      { username: 'tenzing', password, height: 175, jump_height: 20 }
     ]);
+}
--- src/data/seeds/002-goals.js
+++ src/data/seeds/002-goals.js
@@ -3,9 +3,65 @@ exports.seed = knex =>
     .insert([
       {
         user_id: 1,
+        jump_height: 25,
+        start_date: 'January 1st 2019',
+        target_date: 'January 10th 2019',
+        completed: true
+      },
+      {
+        user_id: 1,
+        jump_height: 35,
+        start_date: 'January 10th 2019',
+        target_date: 'January 29th 2019',
+        completed: true
+      },
+      {
+        user_id: 1,
+        jump_height: 38,
+        start_date: 'January 29th 2019',
+        target_date: 'February 5th 2019',
+        completed: true
+      },
+      {
+        user_id: 1,
+        jump_height: 42,
+        start_date: 'February 5th 2019',
+        target_date: 'February 18th 2019',
+        completed: true
+      },
+      {
+        user_id: 1,
         jump_height: 50,
-        start_date: 'April 16th 2019',
-        target_date: 'April 30th 2019',
+        start_date: 'February 18th 2019',
+        target_date: 'March 21st 2019',
+        completed: true
+      },
+      {
+        user_id: 1,
+        jump_height: 53,
+        start_date: 'March 21st 2019',
+        target_date: 'March 28th 2019',
+        completed: true
+      },
+      {
+        user_id: 1,
+        jump_height: 55,
+        start_date: 'March 28th 2019',
+        target_date: 'April 6th 2019',
+        completed: true
+      },
+      {
+        user_id: 1,
+        jump_height: 59,
+        start_date: 'April 6th 2019',
+        target_date: 'April 18th 2019',
+        completed: true
+      },
+      {
+        user_id: 1,
+        jump_height: 65,
+        start_date: 'April 18th 2019',
+        target_date: 'May 1st 2019',
         completed: false
-      }
+      },
     ]);
